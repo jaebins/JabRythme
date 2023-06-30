d910f5d1712dd298cb3bbea9058eeeafb6e3f7db
jaebins <jen2065@naver.com>
2023-06-30 15:18:23 +0900

    d

--- src/components/users.js
+++ src/components/users.js
@@ -3,12 +3,15 @@ export default function Users(){
     window.open(`${process.env.PUBLIC_URL}/setting`, "설정", "width=300px height=700px resizable=no")
   }
 
+  const goLogin = (mode) => {
+    window.location.href = `${process.env.PUBLIC_URL}/${mode}`
+  }
     return(
         <div className="users">
           <div className="users-setting" onClick={() => openSetting()}>설정</div>
           <div className="users-userInfo">
-            <div className="users-userInfo-user">회원가입</div>
-            <div className="users-userInfo-user">로그인</div>
+            <div className="users-userInfo-user" onClick={() => goLogin("/register")}>회원가입</div>
+            <div className="users-userInfo-user" onClick={() => goLogin("/login")}>로그인</div>
           </div>
         </div>
     )
